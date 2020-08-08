3b6e7aa7605893a46d99bad6a33b33d1aa54f862
GeneticaROMD <69114543+GeneticaROMD@users.noreply.github.com>
2020-08-09 00:19:44 +0100

    Added support for other languages

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ window.addEventListener('load', () => {
         element.classList.add("img-fluid");
     });
 });
+
+// Change language on button click
+const language = document.getElementById("language").addEventListener("click", () => {
+    const languageOption = document.getElementById("language").value;
+
+    switch (languageOption) {
+        case "en":
+            window.location.href = "indexEn.html";
+            document.getElementById("index").setAttribute("href", "indexEn.html");
+            document.getElementById("participa").setAttribute("href", "participaEn.html");
+            document.getElementById("database").setAttribute("href", "databaseEn.html");
+            break;
+        
+        case "ro":
+            window.location.href = "index.html";
+            document.getElementById("index").setAttribute("href", "index.html");
+            document.getElementById("participa").setAttribute("href", "participa.html");
+            document.getElementById("database").setAttribute("href", "database.html");
+    }
+
+});
